84de87425f0d885ff87fd7bbf51ad49e092c2567
r-neal-kelly <r.neal.kelly@gmail.com>
2021-07-31 18:09:42 -0400

    reorganized the file structure of the program and made my first type in typescript

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import path from 'path';
+
+import {
+    app,
+    BrowserWindow,
+} from 'electron';
+
+class Window_t {
+    folder: string;
+    window: BrowserWindow;
+
+    constructor (folder: string) {
+        this.folder = folder;
+
+        this.window = new BrowserWindow({
+            fullscreen: true,
+            webPreferences: {
+                preload: path.join(__dirname, this.folder + '/preload.js'),
+            },
+        });
+        this.window.loadFile('./src/windows/' + this.folder + '/index.html');
+        this.window.webContents.openDevTools();
+    }
+};
+
+app.on('ready', async function (): Promise<void> {
+    new Window_t("gallery");
+
+    // windows cannot be created before 'ready' event
+    app.on('activate', async function (): Promise<void> {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            new Window_t("gallery");
+        }
+    });
+});
+
+app.on('window-all-closed', async function (): Promise<void> {
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
