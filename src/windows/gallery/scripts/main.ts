a66832be5042928a3d0e9a6498dab60cb72c5c23
r-neal-kelly <r.neal.kelly@gmail.com>
2021-08-01 00:19:00 -0400

    added a simple loop, you can click on any album to see the big version of Sephy's image and then click again to go back to albums. we haven't implemented the in-between thumbs view yet

--- src/windows/gallery/scripts/main.ts
+++ src/windows/gallery/scripts/main.ts
@@ -1,39 +1,63 @@
 window.addEventListener("DOMContentLoaded", async function (): Promise<void> {
     console.log(process.platform);
 
+    let viewport_albums: HTMLDivElement;
+    let viewport_picture: HTMLDivElement;
+    let picture: HTMLImageElement;
+
     {
         const title: HTMLTitleElement = document.createElement("title");
         document.head.appendChild(title);
         title.innerText = "Sephy!";
     }
     {
-        const viewport_albums: HTMLDivElement = document.createElement("div");
-        document.body.appendChild(viewport_albums);
-        viewport_albums.classList.add("Viewport_Albums");
-        for (let idx = 0; idx < 6; idx += 1) {
-            const album: HTMLDivElement = document.createElement("div");
-            viewport_albums.appendChild(album);
-            album.classList.add("Album");
-            album.addEventListener("click", async function (): Promise<void> {
-                console.log("clicked " + idx);
-            });
-            {
-                const album_text: HTMLDivElement = document.createElement("div");
-                album.appendChild(album_text);
-                album_text.classList.add("Album_Text");
-                album_text.innerText = "An Album";
-
-                const album_peek: HTMLDivElement = document.createElement("div");
-                album.appendChild(album_peek);
-                album_peek.classList.add("Album_Peek");
+        document.body.classList.add("Viewport");
+        {
+            viewport_albums = document.createElement("div");
+            document.body.appendChild(viewport_albums);
+            viewport_albums.classList.add("Viewport_Albums");
+            for (let idx = 0; idx < 6; idx += 1) {
+                const album: HTMLDivElement = document.createElement("div");
+                viewport_albums.appendChild(album);
+                album.classList.add("Album");
+                album.addEventListener("click", async function (): Promise<void> {
+                    viewport_albums.style["display"] = "none";
+                    viewport_picture.style["display"] = "";
+                    picture.setAttribute("src", "../../../pics/sephy.jpg");
+                });
                 {
-                    const album_peek_thumb: HTMLDivElement = document.createElement("div");
-                    album_peek.appendChild(album_peek_thumb);
-                    album_peek_thumb.classList.add("Thumb");
-                    album_peek_thumb.classList.add("Thumb_Tall");
-                    album_peek_thumb.style["border"] = "0";
-                    album_peek_thumb.style["background-image"] = "url('../../../pics/sephy.jpg')";
+                    const album_text: HTMLDivElement = document.createElement("div");
+                    album.appendChild(album_text);
+                    album_text.classList.add("Album_Text");
+                    album_text.innerText = "An Album";
+    
+                    const album_peek: HTMLDivElement = document.createElement("div");
+                    album.appendChild(album_peek);
+                    album_peek.classList.add("Album_Peek");
+                    {
+                        const album_peek_thumb: HTMLDivElement = document.createElement("div");
+                        album_peek.appendChild(album_peek_thumb);
+                        album_peek_thumb.classList.add("Thumb");
+                        album_peek_thumb.classList.add("Thumb_Tall");
+                        album_peek_thumb.style["border"] = "0";
+                        album_peek_thumb.style["background-image"] = "url('../../../pics/sephy.jpg')";
+                    }
                 }
+            }
+        }
+        {
+            viewport_picture = document.createElement("div");
+            document.body.appendChild(viewport_picture);
+            viewport_picture.classList.add("Viewport_Picture");
+            viewport_picture.style["display"] = "none";
+            {
+                picture = document.createElement("img");
+                viewport_picture.appendChild(picture);
+                picture.classList.add("Picture");
+                picture.addEventListener("click", async function (): Promise<void> {
+                    viewport_albums.style["display"] = "";
+                    viewport_picture.style["display"] = "none";
+                });
             }
         }
     }
