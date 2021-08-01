e40635ef572968e160171752efa30c5657338720
r-neal-kelly <r.neal.kelly@gmail.com>
2021-07-31 22:04:38 -0400

    added a style sheet

--- src/windows/gallery/scripts/main.ts
+++ src/windows/gallery/scripts/main.ts
@@ -1,3 +1,15 @@
 window.addEventListener("DOMContentLoaded", async function (): Promise<void> {
     console.log(process.platform);
+
+    {
+        const title: HTMLTitleElement = document.createElement("title");
+        title.innerText = "Sephy!";
+        document.head.appendChild(title);
+    }
+
+    {
+        const header: HTMLHeadingElement = document.createElement("h1");
+        header.innerText = "Welcome to the Gallery!";
+        document.body.appendChild(header);
+    }
 });
