28ace0dd0d7c01286800c5bb25a2ccde2d11c8ac
r-neal-kelly <r.neal.kelly@gmail.com>
2021-08-05 01:32:40 -0400

    added a back button and fully working folder thumbs

--- src/windows/gallery/scripts/js/main.js
+++ src/windows/gallery/scripts/js/main.js
@@ -9,6 +9,33 @@ let picture_viewport;
 
 let picture;
 
+function get_random_int(from = 0, to_inclusive = Number.MAX_SAFE_INTEGER) {
+    return Math.floor(Math.random() * (to_inclusive - from + 1) + from);
+}
+
+async function get_image_size(image_path) {
+    const image = new Image();
+    image.src = image_path;
+
+    return new Promise(function (resolve, reject) {
+        image.onload = function () {
+            resolve({ width: image.width, height: image.height });
+        };
+        image.onerror = function () {
+            resolve({ width: 0, height: 0 });
+        };
+    });
+}
+
+async function is_image_wide(image_path) {
+    const image_size = await get_image_size(image_path);
+    return image_size.width > image_size.height;
+}
+
+async function is_image_tall(image_path) {
+    return !is_image_wide(image_path);
+}
+
 async function create_root_folder() {
     const originals_request = new XMLHttpRequest();
     originals_request.responseType = "json";
@@ -92,6 +119,20 @@ async function create_folder(parent_folder) {
         thumbs_viewport.firstChild.remove();
     }
 
+    if (parent_folder[".."] != parent_folder) {
+        const back_folder = document.createElement("div");
+        thumbs_viewport.appendChild(back_folder);
+        back_folder.classList.add("Back_Folder");
+        back_folder.addEventListener("click", async function() {
+            create_folder(parent_folder[".."]);
+        });
+
+        const back_folder_name = document.createElement("div");
+        back_folder.appendChild(back_folder_name);
+        back_folder_name.classList.add("Back_Folder_Name");
+        back_folder_name.innerText = "Back";
+    }
+
     for (let [idx, child_folder] of parent_folder.folders.entries()) {
         const folder = document.createElement("div");
         thumbs_viewport.appendChild(folder);
@@ -107,35 +148,51 @@ async function create_folder(parent_folder) {
 
         const folder_thumb = document.createElement("div");
         folder.appendChild(folder_thumb);
-        folder_thumb.classList.add("File");
-        folder_thumb.classList.add("File_Tall");
+        folder_thumb.classList.add("Folder_Thumb");
         folder_thumb.style["border"] = "0";
-        folder_thumb.style["background-image"] = "url('./pics/sephy.jpg')";
     }
+    update_child_folder_thumbs(parent_folder);
 
     for (let [idx, child_file] of parent_folder.files.entries()) {
+        const original_path = child_file.path;
+        const thumb_path = parent_folder.thumbs.files[idx].path;
+
         const file = document.createElement("div");
         thumbs_viewport.appendChild(file);
         file.classList.add("File");
-        file.style["background-image"] = `url("${parent_folder.thumbs.files[idx].path}")`;
+        file.style["background-image"] = `url("${thumb_path}")`;
         file.addEventListener("click", async function () {
             thumbs_viewport.style["display"] = "none";
             picture_viewport.style["display"] = "";
-            picture.setAttribute("src", child_file.path);
+            picture.setAttribute("src", original_path);
         });
 
-        const thumb_image = new Image();
-        new Promise(function (resolve, reject) {
-            thumb_image.onload = () => resolve();
-            thumb_image.onerror = () => reject();
-        }).then(function () {
-            if (thumb_image.width >= thumb_image.height) {
-                file.classList.add("File_Wide");
+        if (await is_image_wide(thumb_path)) {
+            file.classList.add("File_Wide");
+        } else {
+            file.classList.add("File_Tall");
+        }
+    }
+}
+
+async function update_child_folder_thumbs(parent_folder) {
+    const child_folder_thumbs = document.querySelectorAll(".Folder_Thumb");
+    for (let [idx, child_folder_thumb] of child_folder_thumbs.entries()) {
+        child_folder_thumb.classList.remove("Folder_Thumb_Wide");
+        child_folder_thumb.classList.remove("Folder_Thumb_Tall");
+
+        const child_folder = parent_folder.folders[idx];
+        if (child_folder.thumbs.files.length > 0) {
+            const folder_thumb_path = child_folder.thumbs.files[get_random_int(0, child_folder.thumbs.files.length - 1)].path;
+            child_folder_thumb.style["background-image"] = `url("${folder_thumb_path}")`;
+            if (await is_image_wide(folder_thumb_path)) {
+                child_folder_thumb.classList.add("Folder_Thumb_Wide");
             } else {
-                file.classList.add("File_Tall");
+                child_folder_thumb.classList.add("Folder_Thumb_Tall");
             }
-        });
-        thumb_image.src = child_file.path;
+        } else {
+            child_folder_thumb.style["background-image"] = "";
+        }
     }
 }
 
@@ -147,4 +204,10 @@ window.addEventListener("DOMContentLoaded", async function () {
     await create_root_folder();
     await create_viewports();
     await create_folder(originals_folder);
+
+    window.setInterval(async function () {
+        if (document.visibilityState === "visible") {
+            await update_child_folder_thumbs(current_folder);
+        }
+    }, 3000);
 });
